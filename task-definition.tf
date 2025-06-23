ff0d2b0d9a2d33955689098d554b5445c6c38cdf
Ubuntu <ubuntu@ip-172-31-17-252.eu-north-1.compute.internal>
2025-06-23 10:21:29 +0000

    CHANGED CPU AND MEMORY CONFIGS

--- task-definition.tf
+++ task-definition.tf
@@ -4,8 +4,8 @@ resource "aws_ecs_task_definition" "strapi" {
   family                   = "${var.project_name}-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "512"
-  memory                   = "1024"
+  cpu                      = "2048"
+  memory                   = "4096"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
