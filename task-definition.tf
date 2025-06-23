e6ac8ecd38a8840446003d55cf02beac5d848acd
Ubuntu <ubuntu@ip-172-31-17-252.eu-north-1.compute.internal>
2025-06-23 11:06:14 +0000

    Add PostgreSQL DATABASE_URL to ECS task definition

--- task-definition.tf
+++ task-definition.tf
@@ -1,5 +1,3 @@
-# terraform/task-definition.tf
-
 resource "aws_ecs_task_definition" "strapi" {
   family                   = "${var.project_name}-task"
   requires_compatibilities = ["FARGATE"]
@@ -8,32 +6,33 @@ resource "aws_ecs_task_definition" "strapi" {
   memory                   = "4096"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-  container_definitions = jsonencode([
-    {
-      name      = "strapi"
-      image     = "adarshdogra1122/strapi-app:v1"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 1337
-          hostPort      = 1337
-          protocol      = "tcp"
-        }
-      ]
-      environment = [
-        {
-          name  = "NODE_ENV"
-          value = "production"
-        }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = "/ecs/strapi"
-          awslogs-region        = var.aws_region
-          awslogs-stream-prefix = "ecs"
-        }
+  container_definitions = jsonencode([{
+    name      = "strapi"
+    image     = "adarshdogra1122/strapi-app:v1"
+    essential = true
+    portMappings = [{
+      containerPort = 1337
+      hostPort      = 1337
+      protocol      = "tcp"
+    }]
+    environment = [
+      {
+        name  = "NODE_ENV"
+        value = "production"
+      },
+      {
+        name  = "DATABASE_URL"
+        value = "postgresql://postgres:rzekqiawoPDLUpiiwpMsIHzocehyRmmP@nozomi.proxy.rlwy.net:20653/railway"
+      }
+    ]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "/ecs/strapi"
+        awslogs-region        = var.aws_region
+        awslogs-stream-prefix = "ecs"
       }
     }
-  ])
+  }])
 }
+
